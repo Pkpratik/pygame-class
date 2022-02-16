0feef2606a9a9f88f3d94e5d217afca34d75ff77
pratik agrawal <46810588+Pkpratik@users.noreply.github.com>
2022-02-17 01:59:55 +0530

    added score

--- piano_tiles/objects.py
+++ piano_tiles/objects.py
@@ -13,15 +13,17 @@ class Tile(pygame.sprite.Sprite):
         self.x=x
         self.y=y
         self.color = (0,0,0)
-        
         self.image = pygame.Surface((tile_width,tile_height),pygame.SRCALPHA )
         self.rect=self.image.get_rect()
         self.rect.x=x
         self.rect.y = y
+        self.alive=True
     def update(self,speed):
         self.rect.y+=speed
         if self.rect.y>=height:
             self.kill()
+        if self.alive==False:
+            self.color=(0,0,0,90)
         pygame.draw.rect(self.image,self.color,(0,0,tile_width,tile_height))
         self.screen.blit(self.image,self.rect)
         
--- piano_tiles/piano.py
+++ piano_tiles/piano.py
@@ -11,31 +11,53 @@ screen = pygame.display.set_mode((width,height))
 
 white=(255,255,255)
 scrolling=0
-num_tile=0
+num_tile=1
 score=0
 
 
 tile_group=pygame.sprite.Group()
-
+x=random.randint(0,3)
+t=Tile(x*tile_width,-tile_height,screen)
+tile_group.add(t)
 fps=30
 speed=2
 run=True
 clock=pygame.time.Clock()
 while run:
-    
+    pos=None
     screen.fill(white)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
-    tile_group.update(speed)
-    if scrolling>=(num_tile*tile_height):
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            pos=event.pos
+    #tile_group.update(speed)
+    for tile in tile_group:
+        tile.update(speed)
+        if pos:
+            if tile.rect.collidepoint(pos):
+                score+=1
+                tile.alive=False
+                print(score)
+        if tile.rect.bottom>=height and tile.alive:
+            run=False
+    if len(tile_group)>0:
+        t=tile_group.sprites()[-1]
+        if t.rect.top+speed>=0:
+            x=random.randint(0,3)
+            y=-tile_height+t.rect.top
+            t=Tile(x*tile_width,y,screen)
+            tile_group.add(t)
+            num_tile+=1
+            tile_group.draw(screen)
+    else:
         x=random.randint(0,3)
-        t=Tile(x*tile_width,-tile_height,screen)
+        y=-tile_height+t.rect.top
+        t=Tile(x*tile_width,y,screen)
         tile_group.add(t)
         num_tile+=1
-        tile_group.draw(screen)
-        score+=1
-    if scrolling%200==0:
+        tile_group.draw(screen)       
+    if scrolling%600==0:
         speed+=1
 
     scrolling+=speed
