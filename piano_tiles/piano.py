089832eac5d5b964731e818474442581313efb3f
pratik agrawal <46810588+Pkpratik@users.noreply.github.com>
2022-02-24 03:27:04 +0530

    added leaderboard

--- piano_tiles/piano.py
+++ piano_tiles/piano.py
@@ -1,3 +1,4 @@
+from operator import le
 import random
 import pygame
 from objects import Tile,mousep
@@ -5,7 +6,7 @@ pygame.init()
 
 #game window size
 height=512
-width=288
+width=300
 screen = pygame.display.set_mode((width,height))
 
 #tile size
@@ -47,6 +48,80 @@ white=(255,255,255)
 
 # game start button
 start_rect= pygame.Rect(75,220,140,52)
+leaderbord_rect= pygame.Rect(75,320,140,52)
+
+def show_hs():
+    back_rect= pygame.Rect(5,5,50,50)
+    namearr=[]
+    scorearr=[]
+    with open("piano_tiles/score.txt","r+") as file:
+        for line in file:
+            hsname,hsscore=line[:line.rindex(" ")],line[line.rindex(" ")+1:]
+            namearr.append(hsname)
+            scorearr.append(hsscore[:-1])
+
+    run=True
+    while run:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                if back_rect.collidepoint(event.pos):
+                    run=False
+        screen.fill((100,100,100))
+        pygame.draw.rect(screen,(150,0,0),back_rect)
+        
+        hsn0 = base_font.render(namearr[0],True,(255,255,255))
+        hsn1 = base_font.render(namearr[1],True,(255,255,255))
+        hsn2 = base_font.render(namearr[2],True,(255,255,255))
+        hsn3 = base_font.render(namearr[3],True,(255,255,255))
+        hsn4 = base_font.render(namearr[4],True,(255,255,255))
+        hsn5 = base_font.render(namearr[5],True,(255,255,255))
+        hsn6 = base_font.render(namearr[6],True,(255,255,255))
+        hsn7 = base_font.render(namearr[7],True,(255,255,255))
+        hsn8 = base_font.render(namearr[8],True,(255,255,255))
+        hsn9 = base_font.render(namearr[9],True,(255,255,255))
+        
+        hss0 = base_font.render(scorearr[0],True,(255,255,255))
+        hss1 = base_font.render(scorearr[1],True,(255,255,255))
+        hss2 = base_font.render(scorearr[2],True,(255,255,255))
+        hss3 = base_font.render(scorearr[3],True,(255,255,255))
+        hss4 = base_font.render(scorearr[4],True,(255,255,255))
+        hss5 = base_font.render(scorearr[5],True,(255,255,255))
+        hss6 = base_font.render(scorearr[6],True,(255,255,255))
+        hss7 = base_font.render(scorearr[7],True,(255,255,255))
+        hss8 = base_font.render(scorearr[8],True,(255,255,255))
+        hss9 = base_font.render(scorearr[9],True,(255,255,255))
+
+        higher_font= pygame.font.Font(None,50)
+        leaderboardtext = higher_font.render("High Scores",True,(255,255,255))
+        screen.blit(leaderboardtext,(60,40))
+
+        screen.blit(hsn0,(25,100))
+        screen.blit(hsn1,(25,130))
+        screen.blit(hsn2,(25,160))
+        screen.blit(hsn3,(25,190))
+        screen.blit(hsn4,(25,220))
+        screen.blit(hsn5,(25,250))
+        screen.blit(hsn6,(25,280))
+        screen.blit(hsn7,(25,310))
+        screen.blit(hsn8,(25,340))
+        screen.blit(hsn9,(25,370))
+
+        screen.blit(hss0,(250,100))
+        screen.blit(hss1,(250,130))
+        screen.blit(hss2,(250,160))
+        screen.blit(hss3,(250,190))
+        screen.blit(hss4,(250,220))
+        screen.blit(hss5,(250,250))
+        screen.blit(hss6,(250,280))
+        screen.blit(hss7,(250,310))
+        screen.blit(hss8,(250,340))
+        screen.blit(hss9,(250,370))
+
+
+
+        pygame.display.update()
 
 
 
@@ -65,7 +140,10 @@ while run:
                 color=color_passive
             if start_rect.collidepoint(pygame.mouse.get_pos()):
                 run=False
-        
+            if leaderbord_rect.collidepoint(event.pos):
+                show_hs()
+
+
         if input_active==True:
             if player_name=="Enter your name":
                 player_name=""
@@ -74,7 +152,8 @@ while run:
                     player_name=player_name[:-1]
                 else:
                     player_name+=event.unicode
-
+                    if len(player_name)>20:
+                        player_name=player_name[:-1]
     screen.fill((100,100,80))
     
     # player name box display
@@ -83,10 +162,12 @@ while run:
     screen.blit(name_surface,(input_rect.x+5,input_rect.y+5))
     input_rect.w = max(name_surface.get_width()+10,140)
     input_rect.x=width//2-input_rect.w//2
-
+    start_rect.x=width//2-start_rect.w//2
+    leaderbord_rect.x=width//2-leaderbord_rect.w//2
 
     #start button display
     pygame.draw.rect(screen,(100,150,100),start_rect)
+    pygame.draw.rect(screen,(100,150,100),leaderbord_rect)
     start_surface = base_font.render("START",True,(255,255,255))
     screen.blit(start_surface,(start_rect.x+33,start_rect.y+15))
 
@@ -166,8 +247,6 @@ hsdict={}
 
 with open("piano_tiles/score.txt","r+") as file:
     for line in file:
-        print(line.rindex(" "))
-        print(line[:line.rindex(" ")],"a")
         line[line.rindex(" "):]
         hsname,hsscore=line[:line.rindex(" ")],int(line[line.rindex(" ")+1:])
         hsdict[hsname]=hsscore
