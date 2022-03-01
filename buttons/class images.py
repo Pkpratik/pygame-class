9e33d84480dacf9a9028e900aad1f2ea4b2bfde1
pratik agrawal <46810588+Pkpratik@users.noreply.github.com>
2022-03-01 13:14:50 +0530

    added a class on showing images

---
+++ buttons/class images.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+width=600
+height=600
+
+bg = (204, 102, 0)
+
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption('Button Demo')
+
+
+button_image=pygame.image.load("buttons\icon2.png")
+row = pygame.transform.scale(button_image, (100,100))
+run=True
+while run:
+    screen.fill(bg)
+    pos=pygame.mouse.get_pos()
+    pygame.draw.rect(screen,(255,0,0),Rect(75,200,100,100))
+    screen.blit(row,Rect(75,200,100,100))
+    # if Rect(75,200,100,100).collidepoint(pos):
+    #     pygame.draw.rect(screen,(75,225,255),Rect(75,200,100,100))
+    # pygame.draw.line(screen, (0,0,0),(75,300),(175,300),2)
+    # pygame.draw.line(screen, (0,0,0),(175,200),(175,300),2)
+    # pygame.draw.line(screen, (255,255,255),(75,200),(175,200),2)
+    # pygame.draw.line(screen, (255,255,255),(75,200),(75,300),2)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run=False
+    pygame.display.update()
+pygame.quit()
