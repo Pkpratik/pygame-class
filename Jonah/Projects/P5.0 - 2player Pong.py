fe012824083b226a6ca32b40861d7739428b0680
Jonah Jimmink <jonahjimmink@gmail.com>
2022-03-05 20:52:17 +0100

    Textfixing - Scores

--- Jonah/Projects/P5.0 - 2player Pong.py
+++ Jonah/Projects/P5.0 - 2player Pong.py
@@ -13,11 +13,29 @@ screen=pygame.display.set_mode((w,h))
 pygame.display.set_caption('Brick Breaker')
 base_font=pygame.font.Font(None,40)
 other_font=pygame.font.Font(None,32)
+starting_font=pygame.font.Font(None,64)
 
 #dif. speeds
 forward_speed=1
 downward_speed=1
 barspeed=8
+
+#Buttons
+starting_button=pygame.Rect(225,200,156,75)
+
+#player1 info
+player_name1="Enter name player1"
+input_rect1=pygame.Rect(70,55,150,32)
+input_active1=False
+deactive_color=(255,0,0)
+active_color=(0,255,0)
+input_color1=deactive_color
+
+#player2 info
+player_name2="Enter name player2"
+input_rect2=pygame.Rect(70,130,150,32)
+input_active2=False
+input_color2=deactive_color
 
 #sounds
 barhit=pygame.mixer.Sound('Jonah/Projects/2pp_barsound.wav')
@@ -106,6 +124,66 @@ class ball(pygame.sprite.Sprite):
     
 ball1=ball()
 
+run=True
+    #Openingsscreen
+while run:
+    #Standard while-loop beginning
+    screen=pygame.display.set_mode((600,600))
+    screen.fill((184,255,184))
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run=False
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            if starting_button.collidepoint(event.pos):
+                pygame.draw.rect(screen,(85,176,85),starting_button)
+                run=False
+            if input_rect1.collidepoint(event.pos):
+                input_active2=False
+                input_active1=True
+                input_color1=active_color
+                if player_name1=="Enter name player1":
+                    player_name1=""
+            else:
+                input_color=deactive_color
+            if input_rect2.collidepoint(event.pos):
+                input_active1=False
+                input_active2=True
+                input_color2=active_color
+                if player_name2=="Enter name player2":
+                    player_name2=""
+            else:
+                input_color=deactive_color
+        
+
+        if event.type==pygame.KEYDOWN:
+            if input_active1==True:
+                if event.key==pygame.K_BACKSPACE:
+                    player_name1=player_name1[:-1]
+                else:
+                    player_name1+=event.unicode
+            if input_active2==True:
+                if event.key==pygame.K_BACKSPACE:
+                    player_name2=player_name2[:-1]
+                else:
+                    player_name2+=event.unicode
+            
+    pygame.draw.rect(screen,(4,126,4),starting_button)
+    pygame.draw.rect(screen,input_color1,input_rect1,4)
+    pygame.draw.rect(screen,input_color2,input_rect2,4)
+    name_surface1=base_font.render(player_name1,True,black)
+    name_surface2=base_font.render(player_name2,True,black)
+    input_rect1.w = max(name_surface1.get_width()+10,140)
+    input_rect1.x=w//2-input_rect1.w//2
+    input_rect2.w = max(name_surface2.get_width()+10,140)
+    input_rect2.x=w//2-input_rect2.w//2
+    screen.blit(name_surface1,(input_rect1.x+5,input_rect1.y+5))
+    screen.blit(name_surface2,(input_rect2.x+5,input_rect2.y+5))
+    
+    #Other
+    someothervariable2=starting_font.render("START",True,black)
+    screen.blit(someothervariable2,(starting_button.x+5,starting_button.y+17))
+    pygame.display.update()
+
 #Gameloop
 run=True
 while run:
@@ -124,8 +202,12 @@ while run:
     if key[pygame.K_LEFT]==1 and bar1.left>=0:
         bar1=pygame.Rect.move(bar1,-barspeed,0)
     if key[pygame.K_SPACE]==1:
-        if ball1.setdown==True or ball1.setup==True:
+        if ball1.setdown==True:
             ball1.teamscore=-1
+            ball1.forward_speed=2
+            ball1.downward_speed=2
+        if ball1.setup==True:
+            ball1.teamscore=0 
             ball1.forward_speed=2
             ball1.downward_speed=2
         ball1.space_pressed=True
@@ -157,16 +239,19 @@ while run:
     screen.blit(sup,(25,250))
     sdown=base_font.render(str(scoredown),True,white)
     screen.blit(sdown,(25,350))
-    steam=other_font.render("Teamscore - "+str(ball1.teamscore),True,white)
-    hsteam=other_font.render("Team Highscore - "+str(ball1.ths),True,white)
-    screen.blit(steam,(425,40))
-    screen.blit(hsteam,(371,10))
-
+    if ball1.setup==True or ball1.setdown==True:
+        steam=other_font.render("Teamscore - "+str(ball1.teamscore),True,white)
+        screen.blit(steam,(425,40))
+        hsteam=other_font.render("Team Highscore - "+str(ball1.ths),True,white)
+        screen.blit(hsteam,(371,10))
+    n1=other_font.render(str(player_name1),True,white)
+    screen.blit(n1,(10,10))
+    n2=other_font.render(str(player_name2),True,white)
+    screen.blit(n2,(10,h-32))
 
     #Overall drawing/displaying
     pygame.draw.rect(screen,white,bar1)
     pygame.draw.rect(screen,white,bar2)
-    pygame.draw.line(screen,white,(0,300),(600,300))
     
     #Standard While-loop ending
     pygame.display.update()
